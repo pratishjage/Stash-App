b04c95a28395100c5814e1e7ffb586ecbcf321de
pratish <pratish.j@pepperfry.com>
2020-12-22 17:43:17 +0530

    Refactoring

--- app/src/main/java/com/app/sampleapp/stash/utils/Constants.kt
+++ app/src/main/java/com/app/sampleapp/stash/utils/Constants.kt
@@ -2,7 +2,6 @@ package com.app.sampleapp.stash.utils
 
 object Constants {
     const val SCREEN_BG = "screen_bg"
-    const val RED = "red"
     const val SCREEN_NUMBER = "screen_number"
     const val FRAGMENT_ONE = 1
     const val FRAGMENT_TWO = 2
@@ -10,6 +9,4 @@ object Constants {
     const val FRAGMENT_FOUR = 4
     const val INITIAL_FRAGMENT = "initial_fragment"
     const val MAXIMUM_STEPS = "maximum_steps"
-    const val INITIAL_FRAGMENT_NAME = "initial_fragment_name"
-
 }
